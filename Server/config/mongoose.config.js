28e3c6e0aa0e69621f08cd293d06aa5e11b34d77
NolanRangel <90576911+NolanRangel@users.noreply.github.com>
2022-07-06 15:00:34 -0600

    idk

--- Server/config/mongoose.config.js
+++ Server/config/mongoose.config.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
     try {
+        console.log(typeof process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
